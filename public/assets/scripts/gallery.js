eafac27008bd2396e62b0ee40115df116fd70243
Антон Истомин <istomanton@gmail.com>
2014-11-17 13:08:59 +0500

    added f5 cookies

--- public/assets/scripts/gallery.js
+++ public/assets/scripts/gallery.js
@@ -1,6 +1,68 @@
-document.i = 0;
-
+document.imgIndex = null;
+addCrossBrowserListener(window, "load", init);
 var gelements = [];
+
+function init() {
+    document.imgIndex = readCookie("imgIndex");
+    addCrossBrowserListener(window, "hashchange", hashChange);
+    if (document.imgIndex) {
+        document.location.hash = document.imgIndex;
+        openPreview(document.imgIndex);
+    }
+}
+
+function hashChange() {
+    console.log("hash changed");
+}
+
+function openPreview(i) {
+    document.location.hash = i;
+    
+    var info = document.createElement("p");
+    info.className = "info";
+    info.id = "info1";
+    info.appendChild(document.createTextNode("Press F1 for help"));
+
+    var closeBar = document.createElement("div");
+    closeBar.className = "closeBar";
+    closeBar.id = "closeBar1";
+    addCrossBrowserListener(closeBar, 'click', closePreview);
+
+    var spinner = document.createElement("img");
+    spinner.className = "spinner";
+    spinner.id = "spinner1";
+    spinner.src = "/images/spinner.gif";
+
+    var img = document.createElement("img");
+    img.className = "img";
+    img.id = "img1";
+    img.onload = show;
+    img.src = crossBrowserGetByClassName("gitem")[i].href;
+//    img.src = target.href;
+//    img.src = "http://crispme.com/wp-content/uploads/26105"+".jpg?pass"; // testing purposes
+
+    document.imgIndex = parseInt(i);
+    createCookie("imgIndex", document.imgIndex);
+
+    var imgWrapper = document.createElement("div");
+    imgWrapper.className = "imgWrapper";
+    imgWrapper.id = "imgWrapper1";
+
+    var screen = document.createElement("div");
+    screen.className = "screen";
+    screen.id = "screen1";
+
+    imgWrapper.appendChild(img);
+    document.body.appendChild(screen);
+    document.body.appendChild(info);
+    document.body.appendChild(imgWrapper);
+    document.body.appendChild(spinner);
+    document.body.appendChild(closeBar);
+
+    gelements.push("screen1", "info1", "imgWrapper1", "spinner1", "closeBar1");
+
+    addCrossBrowserListener(document.body, 'keydown', keyDown);
+}
 
 //noinspection JSUnusedGlobalSymbols
 function onClick(e) {
@@ -10,42 +72,7 @@ function onClick(e) {
         target = target.parentNode;
     }
     
-    var info = document.createElement("p");
-    info.className = "info";
-    info.appendChild(document.createTextNode("Press F1 for help"));
-    
-    var closeBar = document.createElement("div");
-    closeBar.className = "closeBar";
-    addCrossBrowserListener(closeBar, 'click', closePreview);
-
-    var spinner = document.createElement("img");
-    spinner.className = "spinner";
-    spinner.src = "/images/spinner.gif";
-    
-    var img = document.createElement("img");
-    img.className = "img";
-    img.onload = showImage;
-    img.src = target.href;
-//    img.src = "http://crispme.com/wp-content/uploads/26105"+".jpg?pass"; // testing purposes
-    
-    document.i = parseInt(target.id);
-    
-    var imgWrapper = document.createElement("div");
-    imgWrapper.className = "imgWrapper";
-    
-    var screen = document.createElement("div");
-    screen.className = "screen";
-
-    imgWrapper.appendChild(img);
-    document.body.appendChild(screen);
-    document.body.appendChild(info);
-    document.body.appendChild(imgWrapper);
-    document.body.appendChild(spinner);
-    document.body.appendChild(closeBar);
-    
-    gelements.push("screen", "info", "imgWrapper", "spinner", "closeBar");
-    
-    addCrossBrowserListener(document.body, 'keydown', keyDown);
+    openPreview(parseInt(target.id));
     
     return preventDefault(e);
 }
@@ -60,18 +87,20 @@ function keyDown(e) {
     if (e.keyCode == 112) {
         if ("onhelp" in window) // F1 in IE
             {window.onhelp = function () {return false;}}
-        var imgWrapper = crossBrowserGetByClassName("imgWrapper")[0];
+        var imgWrapper = document.getElementById("imgWrapper1");
         if (imgWrapper) {
-            var help = crossBrowserGetByClassName("help")[0];
+            var help = document.getElementById("help1");
             if (help) {
                 imgWrapper.removeChild(help);
             }
             else {
                 help = document.createElement("div");
                 help.className = "help";
-                
+                help.id = "help1";
+
                 var helpText = document.createElement("p");
                 helpText.className = "helpText";
+                helpText.id = "helpText1";
                
                 helpText.appendChild(
                     document.createTextNode("Help: Use arrows to navigate, press Esc or right side to close")
@@ -83,13 +112,14 @@ function keyDown(e) {
         }
         return preventDefault(e);
     }
-    var img = crossBrowserGetByClassName("img")[0];
+    var img = document.getElementById("img1");
     var items = crossBrowserGetByClassName("gitem");
     if (e.keyCode == 37) { //arrow left
         if (img && items) {
-            if (document.i <= 0) document.i = items.length;
-            document.i--;
-            img.src = items.item(document.i).href;
+            if (document.imgIndex <= 0) document.imgIndex = items.length;
+            document.imgIndex--;
+            createCookie("imgIndex", document.imgIndex);
+            img.src = items.item(document.imgIndex).href;
             img.style.visibility = "hidden";
             showSpinner();
         }
@@ -97,56 +127,51 @@ function keyDown(e) {
     }
     if (e.keyCode == 39) { //arrow right
         if (img && items) {
-            document.i++;
-            if (document.i >= items.length) document.i = 0;
-            img.src = items.item(document.i).href;
+            document.imgIndex++;
+            createCookie("imgIndex", document.imgIndex);
+            if (document.imgIndex >= items.length) document.imgIndex = 0;
+            img.src = items.item(document.imgIndex).href;
             img.style.visibility = "hidden";
             showSpinner();
-//            console.log(items.item(document.i).href);
+//            console.log(items.item(document.imgIndex).href);
         }
         return preventDefault(e);
     }
 }
 
 function closePreview() {
+    document.location.hash = "";
+    
     for (var i in gelements) {
-        var elem = crossBrowserGetByClassName(gelements[i])[0];
+        var elem = document.getElementById(gelements[i]);
         if (elem) {
             document.body.removeChild(elem);
         }
     }
+    gelements = [];
     removeCrossBrowserListener(document.body, "keydown", keyDown);
+    eraseCookie("imgIndex");
 }
 
-function showImage(e) {
-    e = e || window.event;
-//    var target = getCrossBrowserTarget(e);
+function show(e) {
     var target = this;
     target.style.visibility = "visible";
-//        console.log("loaded!");
     hideSpinner();
 }
 
 function hideSpinner() {
-    var spinner = crossBrowserGetByClassName("spinner")[0];
+    var spinner = document.getElementById("spinner1");
     if (spinner) {
         spinner.style.visibility = "hidden";
     }
 }
 
 function showSpinner() {
-    var spinner = crossBrowserGetByClassName("spinner")[0];
+    var spinner = document.getElementById("spinner1");
     if (spinner) {
         spinner.style.visibility = "visible";
     }
 }
 
 // TODO list:
-    /* features:
-     show, hide
-     fadeIn, fadeOut
-     slideUp,slideDown
-     animate
-     */
-
     //hasOwnProperty check
--- public/assets/scripts/utils.js
+++ public/assets/scripts/utils.js
@@ -21,23 +21,6 @@ function removeCrossBrowserListener (el, event, func) {
     }
 }
 
-function preloadImage(imgSrc, callback){
-    var objImagePreloader = new Image();
-
-    objImagePreloader.src = imgSrc;
-    if(objImagePreloader.complete){
-        callback();
-        objImagePreloader.onload=function(){};
-    }
-    else{
-        objImagePreloader.onload = function() {
-            callback();
-            //    clear onLoad, IE behaves irratically with animated gifs otherwise
-            objImagePreloader.onload = function(){};
-        }
-    }
-}
-
 function crossBrowserGetByClassName (className) {
     if (document.getElementsByClassName) {
         return document.getElementsByClassName(className);
@@ -54,4 +37,31 @@ function getCrossBrowserTarget(e) {
 function isIE () {
     var myNav = navigator.userAgent.toLowerCase();
     return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+}
+
+/* COOKIES */
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
 }
