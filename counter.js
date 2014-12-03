8b4b103e912d078380c6f125bc7858947dc5ab9c
Антон Истомин <istomanton@gmail.com>
2014-12-03 23:28:34 +0500

    migrated to postgres

--- counter.js
+++ counter.js
@@ -1,34 +1,67 @@
 var fs = require('fs');
 var pg = require('pg');
+var UAParser = require('ua-parser-js');
 
 var data = {};
+var conString = process.env.DATABASE_URL;
 
-exports.init = function () {
-//    fs.readFile('./counter.xml', function(err, xmlText) {
-//        parser.parseString(xmlText, function (err, result) {
-//            data.counter = parseInt(result.counter) || 0; 
-//        });
-//    });
-//    if (!data.entries) data.entries = [];
-};
-
-exports.save = function () {
-//    var xml = builder.buildObject(data);
-//    fs.writeFile("./counter.xml", xml);
-};
+// Initialization
+pgQuery('SELECT browser, version, ip FROM visitors', [], function(result){data.visitors = result.rows});
+pgQuery('SELECT * FROM counter', [], function(result){data.hits = result.rows[0].hits;});
 
 exports.increment = function (request) {
-//    var uid = request.ip + request.headers["user-agent"];
-//    if (data.entries.indexOf(uid) == -1) {
-//        data.entries.push(uid);
-//        data.counter++;
-//    }
-//    this.save();
+    var ip = request.ip;
+    var parsedUa = parseUa(request.headers["user-agent"]);
+    var browser = parsedUa.browser.name;
+    var version = parsedUa.browser.version;
+    
+    if (uniqueHit(browser, version, ip)) {
+        data.hits++;
+        data.visitors.push({browser: browser, version: version, ip: ip});
+
+        pgQuery('UPDATE counter SET hits = $1', [data.hits]);
+        pgQuery('INSERT INTO visitors(browser, version, ip) values ($1, $2, $3)', [browser, version, ip]);
+    }
 };
 
 exports.get = function () {
-    return 0;
+    return data.hits;
 };
+
+exports.eraseStats = function() {
+    pgQuery('DELETE FROM visitors');
+    pgQuery('UPDATE counter SET hits = 0');
+};
+
+function parseUa(ua) {
+    var ownBrowser = [[/(curl|wget)\/((\d+)?[\w\.]+)/i],
+        [UAParser.BROWSER.NAME, UAParser.BROWSER.VERSION, UAParser.BROWSER.MAJOR]];
+    var parser = new UAParser(ua, {browser: ownBrowser});
+    return parser.getResult();
+}
+
+function uniqueHit(browser, version, ip) {
+    if (browser.toLowerCase() == "curl" || browser.toLowerCase() == "wget")
+        return false;
+    for (var i in data.visitors) {
+        if (browser+version+ip == data.visitors[i].browser+data.visitors[i].version+data.visitors[i].ip)
+            return false;
+    }
+    return true;
+}
+
+function pgQuery(query, args, handler) {
+    pg.connect(conString, function (err, client, done) {
+        client.query(query, args, function (err, result) {
+            done();
+            if (err) console.error(err);
+            else {
+                if (handler)
+                    handler(result);
+            }
+        });
+    });
+}
 
 exports.writeRequest = function (request, filename) {
     var cache = [];
@@ -40,7 +73,7 @@ exports.writeRequest = function (request, filename) {
             }
             // Store value in our collection
             cache.push(value);
-        } 
+        }
         return value;
     }));
     cache = null;
--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(function(request, response, next) {
     next();
 });
 
-counter.init();
-
 app.get('/', function(request, response) {
 //    response.send('Hey, something went terribly wrong!')
     response.render('index', {title: 'Home', counter: counter.get()})
@@ -24,19 +22,6 @@ app.get('/about', function(request, response) {
 
 app.get('/gallery', function(request, response) {
     response.render('gallery', {title: 'Gallery', counter: counter.get()})
-});
-
-app.get('/db', function (request, response) {
-    var pg = require('pg');
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.send(result.rows); }
-        });
-    });
 });
 
 app.listen(app.get('port'), function() {
